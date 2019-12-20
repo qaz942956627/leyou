8d5603ecdab0b8aa077bfc2dde9326d97df0a32a
lurenfu <942956627@qq.com>
2019-12-20 18:29:18 +0800

    品牌新增同时增加关联表,文件上传服务,mybatis打印sql配置

--- leyou-upload/src/main/java/com/leyou/upload/service/UploadService.java
+++ leyou-upload/src/main/java/com/leyou/upload/service/UploadService.java
@@ -2,6 +2,7 @@ package com.leyou.upload.service;
 
 import com.github.tobato.fastdfs.domain.StorePath;
 import com.github.tobato.fastdfs.service.FastFileStorageClient;
+import org.apache.commons.lang.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
