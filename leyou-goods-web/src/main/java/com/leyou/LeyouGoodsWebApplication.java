9615dc1bfe95dc211c1fee7c8123a8ffee6ff33b
qaz942956627 <942956627@qq.com>
2020-01-19 23:40:05 +0800

    主站 搜索请求

--- leyou-goods-web/src/main/java/com/leyou/LeyouGoodsWebApplication.java
+++ leyou-goods-web/src/main/java/com/leyou/LeyouGoodsWebApplication.java
@@ -5,6 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
+/**
+ * @author 小卢
+ */
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableFeignClients
